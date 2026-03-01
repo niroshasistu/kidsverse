b8a7a6c3c7fe33f6679c2320ff93f0abe17316c0
niroshasistu <sistunirosha243@gmail.com>
2026-03-02 00:46:45 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);  // ✅ new state
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="navbar">
-      {/* Logo */}
+    <div className={`navbar ${scrolled ? "scrolled" : ""}`}>
       <div className="navbar-logo">
         <Link to="/">KidsVerse</Link>
       </div>
 
-      {/* Navigation Links */}
-      <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/learningzone">Learning</Link></li>
-        <li><Link to="/braingames">Brain Games</Link></li>
-        <li><Link to="/storyworld">Stories</Link></li>
-        <li><Link to="/healthtracker">Health</Link></li>
-        <li><Link to="/creativitystudio">Creativity</Link></li>
-        <li><Link to="/ecozone">Eco Zone</Link></li>
-        <li><Link to="/talentshowcase">Talent</Link></li>
-      </ul>
-    </nav>
+      {/* ✅ Hamburger Icon */}
+      <div 
+        className="hamburger" 
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </div>
+
+      {/* ✅ Add active class when open */}
+     <ul
+  className={`navbar-links ${menuOpen ? "active" : ""}`}
+>
+  <li>
+    <Link to="/" onClick={() => setMenuOpen(false)}>
+      Home
+    </Link>
+  </li>
+
+  <li>
+    <Link to="/learningzone" onClick={() => setMenuOpen(false)}>
+      Learning Zone
+    </Link>
+  </li>
+
+  <li>
+    <Link to="/braingames" onClick={() => setMenuOpen(false)}>
+      Brain Games
+    </Link>
+  </li>
+
+  <li>
+    <Link to="/storyworld" onClick={() => setMenuOpen(false)}>
+      Story World
+    </Link>
+  </li>
+
+  <li>
+    <Link to="/creativitystudio" onClick={() => setMenuOpen(false)}>
+      Creativity Studio
+    </Link>
+  </li>
+</ul>
+<div className="nav-right">
+  <a href="/login" className="login-btn">Login</a>
+  <a href="/signup" className="signup-btn">Sign Up</a>
+</div>
+    </div>
   );
 };
 
